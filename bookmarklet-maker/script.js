9285143485d4c855430c9df4aa7d9752049bbd34
CEGC-7 <percivalgilson@outlook.com>
2025-08-26 11:20:50 +1200

    Update script.js

--- bookmarklet-maker/script.js
+++ bookmarklet-maker/script.js
@@ -6,7 +6,7 @@
       const output = document.getElementById('output');
       output.innerHTML = `
         <p>Drag this to your bookmarks bar:</p>
-        <a class="bookmarklet" href="${bookmarklet}">📌 Run Bookmarklet</a>
+        <a class="bookmarklet" href="${bookmarklet}">Run Bookmarklet</a>
         <p>Or copy the code:</p>
         <input type="text" value="${bookmarklet}" style="width:100%;height:1em;" readonly onclick="this.select()">
       `;
