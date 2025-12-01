62b6b5b200210d5b8ff53e422dda8aaf14c6c154
CEGC <percivalgilson@outlook.com>
2025-12-01 18:27:17 +1300

    Update Delving Deeper clone

--- delving-deeper/justdoit.ps1
+++
@@ -1,6 +0,0 @@
-Get-ChildItem -Path . -Recurse -File | ForEach-Object {
-    $content = Get-Content -Path $_.FullName -Raw
-    $newContent = $content -replace 'ighting-men', 'ighting-men'
-    Set-Content -Path $_.FullName -Value $newContent
-}
-
