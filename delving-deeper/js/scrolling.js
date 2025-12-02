4df9b7353d1519f45f3f7a9677733c0add10e97f
CEGC <percivalgilson@outlook.com>
2025-12-02 20:20:43 +1300

    Update and rename Delving Deeper clone

--- delving-deeper/js/scrolling.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  if (location.hash) {
-    const target = document.querySelector(location.hash);
-    if (target) {
-      target.scrollIntoView({behavior: "auto", block: "start"});
-    }
-  }
-});
